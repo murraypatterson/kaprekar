ffdbcd8f915e8f2229213926b0e238200f1fda7f
Murray Patterson <murray.patterson@gmail.com>
2025-12-10 23:06:18 -0500

    embellish display

--- run.bash
+++ run.bash
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-for i in $(seq 5)
-do
-    echo "n=$i..."
-    python3 fixpoints.py $i > $i.csv
-done
+csv() {
+    column -t -s, -n "$@" | less -F -S -X -K
+}
+
+generate() {
+    for i in $(seq $1)
+    do
+	echo "n=$i..."
+	python3 fixpoints.py $i > $i.csv
+    done
+}
+
+display() {
+    echo
+    for i in $(seq $1)
+    do
+	echo "n=$i"
+	csv $i.csv
+	echo
+    done
+}
+
+# Main
+#----------------------------------------------------------------------
+
+N=5
+generate $N
+display $N
